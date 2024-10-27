f0204485bbf15cab641f5c8a3769227c4ddf3949
Aravind <aravindmunna240@gmail.com>
2024-10-27 10:30:22 +0530

    Login

--- src/main/java/com/paymentapp/system/controller/AuthController.java
+++ src/main/java/com/paymentapp/system/controller/AuthController.java
@@ -1,9 +1,17 @@
 package com.paymentapp.system.controller;
 
+import com.paymentapp.system.entity.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 public class AuthController {
+
+    public ResponseEntity<?> login(@RequestBody User user){
+        return  ResponseEntity.ok(HttpStatus.OK);
+    }
 }
