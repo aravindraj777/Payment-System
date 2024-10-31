5e1f21aa856773b415f06a042a1970869e23d61f
Aravind <aravindmunna240@gmail.com>
2024-10-31 19:40:17 +0530

    Security with jwt dependency and configurations done on spring security

---
+++ src/main/java/com/paymentapp/system/config/SecurityConfig.java
@@ -0,0 +1,59 @@
+package com.paymentapp.system.config;
+
+import com.paymentapp.system.enums.Role;
+import com.paymentapp.system.filter.JWTAuthenticationFilter;
+import com.paymentapp.system.service.jwt.IJWTService;
+import com.paymentapp.system.service.jwt.JWTServiceImpl;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@AllArgsConstructor
+public class SecurityConfig {
+
+
+    private final CustomUserDetailsService customUserDetailsService;
+    private final JWTAuthenticationFilter jwtAuthenticationFilter;
+
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws  Exception{
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize->
+                        authorize
+                                .requestMatchers("/api/v1/auth/**").permitAll()
+                                .requestMatchers("/api/v1/payment/**").hasAuthority(Role.USER.name())
+                                .requestMatchers("/api/v1/admin/**").hasAuthority(Role.ADMIN.name())
+                                .anyRequest().authenticated()
+                )
+                .userDetailsService(customUserDetailsService)
+                .sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return  configuration.getAuthenticationManager();
+    }
+
+}
